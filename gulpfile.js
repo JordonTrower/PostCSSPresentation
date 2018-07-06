079778819f73691ef3c48fd1af024405cb5f1066
Jon_White <jonwhite1993@gmail.com>
2018-07-06 13:55:58 -0600

    yolo

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,31 @@ const rucksack = require('rucksack-css');
 const postcssPresetEnv = require('postcss-preset-env');
 
 
+
 gulp.task('default', () => {
 
 	const plugins = [
 		precss(),
 		autoPrefixer(),
 		rucksack(),
-		postcssPresetEnv()
+		postcssPresetEnv(),
+		trolling({
+			aling: false,
+			blur: false,
+			blurBlink: false,
+			clearfix: false,
+			comicSans: false,
+			heigth: false,
+			hideCursor: false,
+			hideOdd: false,
+			ms: false,
+			ren: false,
+			rotate: false,
+			slowlyGrowText: false,
+			verImportant: false,
+			wait: false,
+			zIndex: false
+		})
 	]
 
 	return gulp.src('./src/css/*.css')
